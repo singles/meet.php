bd0b419650b53d9a113ae99dd9e3f3e6a9423999
Radosław Benkel <radoslaw.benkel@gmail.com>
2011-11-07 18:20:14 +0100

    Updated first meeting's agenda

--- app/models/Events.php
+++ app/models/Events.php
@@ -14,15 +14,8 @@ class Events
             ),
             'talks' => array(
                 array(
-                    'topic'                     => 'TBA',
-                    'description'               => 'TBA',
-                    'speaker_name'              => 'Radosław Benkel',
-                    'speaker_avatar'            => 'https://twimg0-a.akamaihd.net/profile_images/1605720400/304109_265882293444507_100000682142964_858395_2624703_n_normal.jpg',
-                    'speaker_www'               => 'http://www.twitter.com/singlespl',
-                ),
-                array(
-                    'topic'                     => 'TBA',
-                    'description'               => 'TBA',
+                    'topic'                     => 'Iteratory',
+                    'description'               => 'Co to? Po co? Jak?',
                     'speaker_name'              => 'Łukasz Kużyński',
                     'speaker_avatar'            => 'https://twimg0-a.akamaihd.net/profile_images/1448595217/24daa83cd30cf8185048483f4e915e88_normal.jpeg',
                     'speaker_www'               => 'http://www.twitter.com/wookiebpl',
@@ -33,6 +26,13 @@ class Events
                     'speaker_name'              => 'Krzysztof Hasiński',
                     'speaker_avatar'            => 'https://lh3.googleusercontent.com/-j2-fvP6eQb8/AAAAAAAAAAI/AAAAAAAAAes/w7aeGL-HlGE/photo.jpg?sz=48',
                     'speaker_www'               => 'https://plus.google.com/104190162645211265692/posts',
+                ),
+                array(
+                    'topic'                     => 'PHP Tips & Tricks',
+                    'description'               => 'Krótkie fragmenty kodu ułatwiające pracę, oraz możliwości języka, z których nie wszyscy zdają sobie sprawę',
+                    'speaker_name'              => 'Radosław Benkel',
+                    'speaker_avatar'            => 'https://twimg0-a.akamaihd.net/profile_images/1605720400/304109_265882293444507_100000682142964_858395_2624703_n_normal.jpg',
+                    'speaker_www'               => 'http://blog.rbenkel.me',
                 ),
             )
         )
