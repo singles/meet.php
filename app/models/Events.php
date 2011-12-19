dec26f51c3ae63291674bdff3fefb434e8836df7
Radosław Benkel <radoslaw.benkel@gmail.com>
2011-12-19 10:40:15 +0100

    Added links to first meet.php presentations - three of them

--- app/models/Events.php
+++ app/models/Events.php
@@ -19,7 +19,7 @@ class Events
                     'speaker_name'              => 'Łukasz Kużyński',
                     'speaker_avatar'            => 'https://twimg0-a.akamaihd.net/profile_images/1448595217/24daa83cd30cf8185048483f4e915e88_normal.jpeg',
                     'speaker_www'               => 'http://www.twitter.com/wookiebpl',
-                    'slides_link'               => '',
+                    'slides_link'               => 'http://www.slideshare.net/wookieb/iteratory',
                 ),
                 array(
                     'topic'                     => 'HipHop for PHP',
@@ -31,19 +31,19 @@ class Events
                 ),
                 array(
                     'topic'                     => 'Zend Framework',
-                    'description'               => 'Dlaczego w PHP dobry framework do podstawa. Kilka informacji o tym jak szybko nauczyć się korzystać z Zend Framework\'a. Dobre praktyki w ZF. Nowości w Zend Framework 2.',
+                    'description'               => 'Dlaczego w PHP dobry framework do podstawa. Kilka informacji o tym jak szybko nauczyć się korzystać z Zend Framework\'a. Dobre praktyki w ZF.',
                     'speaker_name'              => 'Kasper Warguła',
                     'speaker_avatar'            => 'https://lh6.googleusercontent.com/-H8h0EZ63K0g/AAAAAAAAAAI/AAAAAAAABw8/JTb4jtL68Sc/photo.jpg?sz=48',
                     'speaker_www'               => 'https://plus.google.com/109930207969125341657/posts',
-                    'slides_link'               => '',
+                    'slides_link'               => 'http://webstrict.pl/zend/',
                 ),
                 array(
                     'topic'                     => 'PHP Tips & Tricks',
                     'description'               => 'Krótkie fragmenty kodu ułatwiające pracę, rozwiązaujące często spotykane problemy, oraz możliwości języka, z których nie wszyscy zdają sobie sprawę. Niewykluczona dyskusja na temat rozwiązań:).',
                     'speaker_name'              => 'Radosław Benkel',
                     'speaker_avatar'            => 'https://twimg0-a.akamaihd.net/profile_images/1605720400/304109_265882293444507_100000682142964_858395_2624703_n_normal.jpg',
-                    'speaker_www'               => 'http://blog.rbenkel.me',
-                    'slides_link'               => '',
+                    'speaker_www'               => 'http://www.rbenkel.me',
+                    'slides_link'               => 'http://www.slideshare.net/singlespl/php-tips-tricks',
                 )                
             )
         )
