cbede89715459c8e9e423dd2bbd54d20e9ca689d
Radosław Benkel <radoslaw.benkel@gmail.com>
2012-05-24 19:02:44 +0200

    Added 5th meeting slides links

--- app/models/Events.php
+++ app/models/Events.php
@@ -210,7 +210,7 @@ class Events extends Base
                     'speaker_avatar'            => 'http://media.linkedin.com/mpr/mpr/shrink_80_80/p/3/000/012/067/386ba7d.jpg',
                     'speaker_www'               => 'http://piotr.gabryjeluk.pl',
                     'slides_link'               => null,
-                    'skill_level'               => 2
+                    'skill_level'               => 'http://www.slideshare.net/piotrgabryjeluk1/twig-nieze-widoki-dla-php'
                 ),
                 array(
                     'topic'                     => 'Zend Framework 2',
@@ -219,7 +219,7 @@ class Events extends Base
                     'speaker_avatar'            => 'http://www.gravatar.com/avatar/ea7cfe237f4bf563a30da6b9e3ed3977.png',
                     'speaker_www'               => 'http://www.goldenline.pl/mateusz-tymek',
                     'slides_link'               => null,
-                    'skill_level'               => 3
+                    'skill_level'               => 'http://www.slideshare.net/MateuszTymek/zend-framework-2-basic-components'
                 ),
                 array(
                     'topic'                     => 'Wprowadzenie do DI',
@@ -228,7 +228,7 @@ class Events extends Base
                     'speaker_avatar'            => 'https://twimg0-a.akamaihd.net/profile_images/1605720400/304109_265882293444507_100000682142964_858395_2624703_n_normal.jpg',
                     'speaker_www'               => 'http://www.rbenkel.me',
                     'slides_link'               => null,
-                    'skill_level'               => 3
+                    'skill_level'               => 'http://www.slideshare.net/singlespl/introduction-to-dic-13017909'
                 ),
                 array(
                     'topic'                     => 'Phalcon PHP - The fastest PHP framework yet.',
@@ -237,7 +237,7 @@ class Events extends Base
                     'speaker_avatar'            => 'http://dl.dropbox.com/u/968868/841abbef94.jpeg',
                     'speaker_www'               => 'http://www.twitter.com/mailopl',
                     'slides_link'               => null,
-                    'skill_level'               => 2
+                    'skill_level'               => 'http://dl.dropbox.com/u/968868/phalcon.html'
                 )
             )
         )
